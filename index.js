e0fd3d18ca708798c0a368ac75312d71c0dd8d08
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-15 13:30:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,13 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials.map(capitlized);
-
-   function capitlized(tutorial) {
-     tutorial = tutorial.split(' ');
-     for (let i = 0; i < tutorial.length; i++) {
-       tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1).toLowerCase();
-     }
-     return tutorial.join(' ');
-   }
+  function x(tutorial) {
+    tutorial = tutorial.split(" ");
+    for (let i = 0; i < tutorial.length; i++) {
+      tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
+    }
+    return tutorial.join(' ');
+  }
+  return tutorials.map(x);
 }
